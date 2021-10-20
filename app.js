4291c5e6ab4b9bd96d99f7f3f7e3f5ed4d134b41
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-20 16:17:56 -0700

    pokemon randomly generate on page load

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-// import pokemon from './data/pokemon.js';
+import pokemon from './data/pokemon.js';
 // import { findByID } from './utils.js';
 const catchBtn = document.getElementById('catch-button');
 const errorMessage = document.getElementById('error');
+const pokemon1Img = document.getElementById('poke-1-img');
+const pokemon2Img = document.getElementById('poke-2-img');
+const pokemon3Img = document.getElementById('poke-3-img');
 // const playCountSpan = document.getElementById('play-count');
 
 catchBtn.addEventListener('click', ()=>{
@@ -11,19 +14,26 @@ catchBtn.addEventListener('click', ()=>{
     }
 });
 
+const generatePokemon = () =>{
+    let randNum1 = Math.floor(Math.random() * pokemon.length);
+    let randNum2 = Math.floor(Math.random() * pokemon.length);
+    let randNum3 = Math.floor(Math.random() * pokemon.length);
 
-// const generatePokemon = () =>{
-//     let randNum1 = Math.floor(Math.random() * pokemon.length);
-//     let randNum2 = Math.floor(Math.random() * pokemon.length);
-//     let randNum3 = Math.floor(Math.random() * pokemon.length);
+    while (
+        randNum1 === randNum2 || 
+      randNum1 === randNum3 || 
+      randNum2 === randNum3
+    ) {
+        randNum1 = Math.floor(Math.random() * pokemon.length);
+        randNum2 = Math.floor(Math.random() * pokemon.length);
+        randNum3 = Math.floor(Math.random() * pokemon.length);    
+    }
 
-//     while (
-//         randNum1 === randNum2 || 
-//       randNum1 === randNum3 || 
-//       randNum2 === randNum3
-//     ) {
-//         randNum1 = Math.floor(Math.random() * pokemon.length);
-//         randNum2 = Math.floor(Math.random() * pokemon.length);
-//         randNum3 = Math.floor(Math.random() * pokemon.length);      
-//     }
-// };
+    let pokemon1 = pokemon[randNum1];
+    pokemon1Img.src = pokemon1.url_image;
+    let pokemon2 = pokemon[randNum2];
+    pokemon2Img.src = pokemon2.url_image;
+    let pokemon3 = pokemon[randNum3];
+    pokemon3Img.src = pokemon3.url_image;
+};
+generatePokemon();
--- utils.js
+++ utils.js
@@ -5,19 +5,3 @@ export function findByID(id, pokemon){
         }
     }
 }
-
-// export const generatePokemon = () =>{
-//     let randNum1 = Math.floor(Math.random() * 14);
-//     let randNum2 = Math.floor(Math.random() * 14);
-//     let randNum3 = Math.floor(Math.random() * 14);
-
-//     while (
-//         randNum1 === randNum2 || 
-//         randNum1 === randNum3 || 
-//         randNum2 === randNum3
-//     ) {
-//         randNum1 = Math.floor(Math.random() * 14);
-//         randNum2 = Math.floor(Math.random() * 14);
-//         randNum3 = Math.floor(Math.random() * 14);
-//     }
-// };
