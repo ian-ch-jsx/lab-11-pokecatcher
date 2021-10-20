d2a70001d996cb03b1f69c568b0cc90dbb9e8cd7
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-20 15:23:53 -0700

    catch button does something.

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-// import pokemon from '../data/pokemon.js';
+// import pokemon from './data/pokemon.js';
+// import { findByID } from './utils.js';
+const catchBtn = document.getElementById('catch-button');
+// const playCountSpan = document.getElementById('play-count');
+
+catchBtn.addEventListener('click', ()=>{
+    const selected = document.querySelector('input[type=radio]:checked');
+    if (!selected) {
+        console.log('help');
+    }
+});
+
+
+
+
+// const generatePokemon = () =>{
+//     let randNum1 = Math.floor(Math.random() * pokemon.length);
+//     let randNum2 = Math.floor(Math.random() * pokemon.length);
+//     let randNum3 = Math.floor(Math.random() * pokemon.length);
+
+//     while (
+//         randNum1 === randNum2 || 
+//       randNum1 === randNum3 || 
+//       randNum2 === randNum3
+//     ) {
+//         randNum1 = Math.floor(Math.random() * pokemon.length);
+//         randNum2 = Math.floor(Math.random() * pokemon.length);
+//         randNum3 = Math.floor(Math.random() * pokemon.length);      
+//     }
+// };
