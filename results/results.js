f31360ea46fd15a9efd9278e236a41c1e6e4d104
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-22 15:19:38 -0700

    functioning chart

--- results/results.js
+++ results/results.js
@@ -34,9 +34,12 @@ const name = pokedex.map((species)=>{
     return poke.pokemon;
 
 });
-console.log(name);
 const caught = pokedex.map(species=>species.caught);
-console.log(caught);
+
+const encountered = pokedex.map(species=>species.encountered);
+
+// console.log(caught);
+// console.log(encountered);
 var ctx = document.getElementById('resultsChart').getContext('2d');
 // eslint-disable-next-line no-undef
 new Chart(ctx, {
@@ -63,12 +66,35 @@ new Chart(ctx, {
                 'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
+        },
+        {
+            label: 'Pokemon encountered',
+            data: encountered,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
         }]
     },
     options: {
+        indexAxis: 'y',
         scales: {
             y: {
-                beginAtZero: true
+                beginAtZero: true,
+                stacked: false
             }
         }
     }
