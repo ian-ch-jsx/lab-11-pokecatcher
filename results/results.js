af8a2ef1ece2468295de885bb3f3c528e7bdc5e0
Ian Christiansen <ianc.songdog@gmail.com>
2021-10-22 15:01:57 -0700

    names displaying on chart

--- results/results.js
+++ results/results.js
@@ -28,3 +28,48 @@ for (let species of pokedex){
     resultsSpan3.append(img);
     main.append(div);
 }
+
+const name = pokedex.map((species)=>{
+    const poke = findByID(pokemon, species.id);
+    return poke.pokemon;
+
+});
+console.log(name);
+const caught = pokedex.map(species=>species.picked);
+
+var ctx = document.getElementById('resultsChart').getContext('2d');
+// eslint-disable-next-line no-undef
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: name,
+        datasets: [{
+            label: 'Pokemon Caught',
+            data: caught,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
